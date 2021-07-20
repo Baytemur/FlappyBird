95cabc93ca56bbd92cf534f1320715db510b1742
Baytemur <brfnbytmr@gmail.com>
2021-07-20 09:07:34 +0300

    Game Release

---
+++ Assets/Scripts/KusHareketler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KusHareketler : MonoBehaviour
+{
+    public float yercekimi = 4f;
+    public float ziplamaGucu = 2.5f;
+    public float yatayHiz = 1.5f;
+    public float dikeyHiz = 0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        dikeyHiz -= yercekimi * Time.deltaTime;
+        if (Input.GetMouseButtonDown(0)) ;
+        dikeyHiz = ziplamaGucu;
+
+        float egim = 90 * dikeyHiz / yatayHiz;
+
+        if (egim < -50) egim = -50;
+        else if (egim > 50) egim = 50;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, egim);
+        transform.Translate(new Vector3(0, dikeyHiz, 0) *Time.deltaTime, Space.World);
+
+        
+    }
+}
---
+++ Assets/Scripts/ilkKus.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ilkKus : MonoBehaviour
+{
+    public int saniyedeKareSayisi = 10;
+    public Sprite[] animasyonKareleri;
+
+    private SpriteRenderer SpriteRenderer;
+
+    private float sonrakiAnimasyonDegismeAni;
+    private bool animasyonYonu = true;
+    private int mevcutAnimasyonKaresi = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (animasyonKareleri.Length < 2)
+            Destroy(this);
+
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+        sonrakiAnimasyonDegismeAni = Time.time + 1f / saniyedeKareSayisi;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Time.time >= sonrakiAnimasyonDegismeAni )
+        {
+            if (animasyonYonu)
+            {
+                if (mevcutAnimasyonKaresi == animasyonKareleri.Length - 1)
+                {
+                    mevcutAnimasyonKaresi--;
+                    animasyonYonu = false;
+                }
+                else
+                {
+                    mevcutAnimasyonKaresi++;
+                }
+            }
+            else
+            {
+                if (mevcutAnimasyonKaresi == 0 )
+                {
+                    mevcutAnimasyonKaresi++;
+                    animasyonYonu = true;
+                }
+                else
+                {
+                    mevcutAnimasyonKaresi--;
+                }
+            }
+            SpriteRenderer.sprite = animasyonKareleri[mevcutAnimasyonKaresi];
+            sonrakiAnimasyonDegismeAni = Time.time + 1f / saniyedeKareSayisi;
+            }
+        }
+    }
